ce4e5e559696b2b3f33777205544d7d592eb293a
Rinto Jose <839655roje@01HW483720.local>
2016-02-18 21:01:59 +0530

    Checkin @ Thu Feb 18 21:01:59 IST 2016

--- gulpfile.paths.js
+++ gulpfile.paths.js
@@ -8,7 +8,9 @@ let baseLibs = [
 	'node_modules/angular2/bundles/angular2.dev.js',
 	'node_modules/angular2/bundles/router.dev.js',
 	'node_modules/angular2/bundles/http.dev.js',
-	'node_modules/lodash/index.js'
+	'node_modules/lodash/index.js',
+	'node_modules/jquery/dist/jquery.js',
+	'node_modules/materialize-css/dist/js/materialize.js'
 ];
 
 module.exports = {
