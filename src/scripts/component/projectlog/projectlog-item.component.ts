4c8959ca83a0cf7cc07e2a3da787a30632967c54
rintoj <rintoj@gmail.com>
2016-03-08 00:09:22 +0530

    Checkin @ Tue Mar  8 00:09:22 IST 2016

--- src/scripts/component/projectlog/projectlog-item.component.ts
+++ src/scripts/component/projectlog/projectlog-item.component.ts
@@ -3,38 +3,33 @@ import {Projectlog} from '../../state/projectlog';
 import {Component, View, Input, Output, EventEmitter} from 'angular2/core';
 
 @Component({
-  selector: 'pw-projectlog-item',
-  host: {
-    '[class.open]': 'item.uiState.open',
-    '[class.selected-item]': 'item.uiState.selected',
-    '(click)': 'toggleOpen($event)'
-  }
+  selector: 'pw-projectlog-item'
 })
 @View({
   template: `
-		<!-- the list item-->
-		<div class="avatar fa" (click)="toggleSelection($event)">{{item.title.substr(0, 1).toUpperCase()}}</div>
+		<div class="list-item" (click)="toggleOpen($event)" [class.open]="item.uiState.open"
+			[class.selected-item]="item.uiState.selected" [class.edit-mode]="editMode">
+			<!-- the list item-->
+			<div class="avatar fa" (click)="toggleSelection($event)">{{item.title.substr(0, 1).toUpperCase()}}</div>
 
-		<div class="content">
-			<div class="heading-row">
-				<div class="heading">{{item.title}}</div>
+			<div class="content">
+				<div class="heading-row">
+					<div class="heading">{{item.title}}</div>
+				</div>
+
+				<div class="text">
+					<div class="status">{{item.index}}</div>
+					<div class="status" [class.grey-text]="item.status==='done'" [class.green-text]="item.status==='doing'"
+					 [class.yellow-text]="item.status==='new'">{{item.status}}</div>
+				 	<div class="id">{{item.id}}</div>
+				</div>
+
+				<span class="description">{{item.description}}</span>
 			</div>
-
-			<div class="text">
-				<div class="separator"></div>
-				<div class="status">{{item.index}}</div>
-				<div class="status" [class.grey-text]="item.status==='done'" [class.green-text]="item.status==='doing'"
-				 [class.yellow-text]="item.status==='new'">{{item.status}}</div>
-			 	<div class="id">{{item.id}}</div>
-
-				<div class="separator"></div>
-
-				{{item.description}}
+			<div class="action-bar">
+				<a class="fa fa-edit" (click)="toggleEdit()"></a>
+				<a class="fa fa-trash"></a>
 			</div>
-		</div>
-		<div class="action-bar">
-			<a class="fa fa-edit"></a>
-			<a class="fa fa-trash"></a>
 		</div>
 	`
 })
@@ -42,6 +37,8 @@ export class ProjectlogItemComponent {
 
   @Input() item: Projectlog;
   @Output() statusUpdate: EventEmitter<Projectlog> = new EventEmitter<Projectlog>();
+
+	private editMode:boolean = false;
 
   constructor(private service: ProjectlogService) { }
 
@@ -60,5 +57,9 @@ export class ProjectlogItemComponent {
     this.item.uiState.selected = !this.item.uiState.selected;
     this.fireStatusUpdate();
   }
+
+	toggleEdit() {
+		this.editMode = !this.editMode;
+	}
 
 }
